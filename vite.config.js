5a5150129a9ac277be88bf2068b9030710007809
draju <draju@aptean.com>
2025-07-06 12:35:02 +0530

    Add .gitignore, create index.html, update gh-pages version, and adjust Vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dhanushRaju.github.io/", // Set base for GitHub Pages
   plugins: [react()],
+  base: "/",
   css: {
     postcss: "./postcss.config.js",
   },
